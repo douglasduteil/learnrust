818a1bca56327dfb59202321daea3f97de5dc19b
Douglas Duteil <douglasduteil@gmail.com>
2018-01-05 15:15:35 +0100

    chore(ci): pass all arguments after pipefail

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -exu pipefail
+set -exu pipefail $@
 
 mkdir -p releases
 
-for TARGET in "$@"
+for TARGET in "${@:2}"
 do
   cargo build --target $TARGET --release --verbose
   tar Jcvf releases/learnrust-stable-$TARGET.tar.xz target/$TARGET/release/learnrust
